d9df5b5acb692bf65c7db6450f67013c4ecfd82b
GSW-11 <74748868+GSW-11@users.noreply.github.com>
2023-10-12 14:24:22 +0800

    Update hupu_my_more.js

--- Script/hupu_my_more.js
+++ Script/hupu_my_more.js
@@ -1,5 +1,7 @@
 let data = $response.body;
+console.log("data--->"+data)
 let json = JSON.parse(data)
+console.log("json--->"+json)
 json.result.vipInfo.iconText = "";
 json.result.vipInfo.textInfo = "";
 let body = JSON.stringify(json)
